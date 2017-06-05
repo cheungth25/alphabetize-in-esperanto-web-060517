4ba347538b0543a92b66c0b927836e4ce29bcc23
Thomas Cheung <cheungth25@gmail.com>
2017-06-05 03:54:35 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+  arr.sort_by { |string| string.split("").map { |character| ESPERANTO_ALPHABET.index(character) }}
 end
